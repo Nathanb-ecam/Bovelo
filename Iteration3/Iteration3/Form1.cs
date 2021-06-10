3a0bd84c70e958ea6062d44c93fac8410af08c0b
heni974 <heni.rekik8@gmail.com>
2021-06-10 23:32:57 +0200

    working on stock function

--- Iteration3/Iteration3/Form1.cs
+++ Iteration3/Iteration3/Form1.cs
@@ -24,17 +24,34 @@ namespace Iteration3
 
         private void Stock()
         {
-            MySqlCommand command = new MySqlCommand(String.Format("SELECT * FROM PartsToOrder"), cn);
+            MySqlCommand command = new MySqlCommand(String.Format("SELECT * FROM TestStock"), cn);
             MySqlDataReader reader = command.ExecuteReader();
             DataTable dt = new DataTable();
             dt.Load(reader);
+            // here we want to filter the cases and propose quantity values to help the manager by adding a new colum 'suggestion'
+            DataColumn c = new DataColumn();
+            c.DataType = typeof(Int32);
+            c.ColumnName = "Suggestion";
+            dt.Columns.Add(c);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                // ... A CHANGER ...
+                int quantity = Int32.Parse(row["quantity"].ToString());
+                int min = Int32.Parse(row["min"].ToString());
+                if (quantity < min)
+                {
+                    row["Suggestion"] = (min-quantity).ToString();
+                    
+                }            
+            }
+
+            // we display the grid with the values
             stockGrid.DataSource = dt;
         }
 
         private void refillBtn_Click(object sender, EventArgs e)
-        {
-            
-            
+        {      
             // this part updates Stock table with the values entered
             DataTable dt = (DataTable)stockGrid.DataSource;
             foreach (DataRow row in dt.Rows)
@@ -42,9 +59,15 @@ namespace Iteration3
             {
                 string name = row["name"].ToString();
                 int quantity = Int32.Parse(row["quantity"].ToString());
-                MySqlCommand cmd = new MySqlCommand(String.Format("Update PartsToOrder SET name = @name, quantity=@quantity where name=@name", name), cn);
+                string type = row["Type"].ToString();
+                int size = Int32.Parse(row["size"].ToString());
+                string color = row["Color"].ToString();
+                MySqlCommand cmd = new MySqlCommand(String.Format("Update {0}TestStock SET name = @name, quantity=@quantity where name=@name and color=@color and size=@size and type=@type",type), cn);
                 cmd.Parameters.AddWithValue("@name", name);
                 cmd.Parameters.AddWithValue("@quantity", quantity);
+                cmd.Parameters.AddWithValue("@type", type);
+                cmd.Parameters.AddWithValue("@size", size);
+                cmd.Parameters.AddWithValue("@color", color);
                 cmd.ExecuteNonQuery();
 
             }
